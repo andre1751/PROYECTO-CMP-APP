bdf4d89d1d762aabb410a372be222c3ddb046a07
dyln05 <148879351+dyln05@users.noreply.github.com>
2026-05-01 17:19:30 -0500

    Add fields to Huesped entity class

--- src/main/java/com/example/demo/Entity/Huesped.java
+++ src/main/java/com/example/demo/Entity/Huesped.java
@@ -1,5 +1,18 @@
 package com.example.demo.Entity;
 
 public class Huesped {
+  @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 100, nullable = false)
+    private String nombre;
+
+    @Column(name = "documento_id", length = 20, unique = true, nullable = false)
+    private String documentoId;
+
+    @Column(length = 100)
+    private String email;
+}
 
 }
