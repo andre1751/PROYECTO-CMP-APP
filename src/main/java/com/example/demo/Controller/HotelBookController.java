b654e9dc8e5afe4f56ee0a3f16ed28f73e7fa9e5
JCNoven <extranovi9@gmail.com>
2026-05-02 16:21:23 -0500

    Creación de controller

---
+++ src/main/java/com/example/demo/Controller/HotelBookController.java
@@ -0,0 +1,29 @@
+package com.example.demo.Controller;
+
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Dto.BookingDto;
+import com.example.demo.Entity.Habitacion;
+import com.example.demo.Entity.Huesped;
+import com.example.demo.Entity.Reserva;
+
+
+@RestController
+@RequestMapping("api/todo")
+public class HotelBookController {
+
+    //TO-DO: todas las reservas
+
+    //TO-DO: buscar reserva
+
+    //TO-DO: cancelar reserva
+
+    //TO-DO: Actualizar estado reserva
+
+    //TO-DO: crear nueva reserva
+}
