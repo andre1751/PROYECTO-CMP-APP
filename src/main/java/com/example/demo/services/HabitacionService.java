d6dfcc77ff0c6636a8fca59e62cff1299be2d2c5
andre1751 <andrerosales1751@gmail.com>
2026-05-03 21:16:10 -0500

    Update HabitacionService.java

--- src/main/java/com/example/demo/services/HabitacionService.java
+++ src/main/java/com/example/demo/services/HabitacionService.java
@@ -1,26 +1,33 @@
 package com.example.demo.services;
-
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Habitacion;
 import com.example.demo.repository.HabitacionRepo;
-import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
-public class HabitacionService {
-    private final HabitacionRepo habitacionRepository;
 
+public class HabitacionService {
+
+    private static HabitacionService instance;
+    @Autowired
+
+    private HabitacionRepo habitacionRepository;
+    private HabitacionService() {
+    }
+    public static HabitacionService getInstance() {
+        if (instance == null) {
+            instance = new HabitacionService();
+        }
+        return instance;
+    }
     public List<Habitacion> obtenerTodas() {
         return habitacionRepository.findAll();
     }
-
     public Habitacion buscarPorNumero(String numero) {
         return habitacionRepository.findByNumero(numero)
                 .orElseThrow(() -> new RuntimeException("La habitación " + numero + " no existe."));
     }
-
     public Habitacion actualizarEstado(String numero, String nuevoEstado) {
         Habitacion hab = buscarPorNumero(numero);
         hab.setEstado(nuevoEstado);
