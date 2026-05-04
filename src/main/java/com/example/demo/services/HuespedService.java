38991ad6f45cc544089f143d017629d2de6bad8d
andre1751 <andrerosales1751@gmail.com>
2026-05-03 21:16:34 -0500

    Update HuespedService.java

--- src/main/java/com/example/demo/services/HuespedService.java
+++ src/main/java/com/example/demo/services/HuespedService.java
@@ -1,22 +1,19 @@
 package com.example.demo.services;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.Huesped;
 import com.example.demo.repository.HuespedRepo;
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
-public class HuespedService {
-    
-    private final HuespedRepo huespedRepository;
 
+public class HuespedService {
+@Autowired
+    private HuespedRepo huespedRepository;
     public Huesped obtenerHuesped(Integer documentoId) {
         return huespedRepository.findByDocumentoId(documentoId)
                 .orElseThrow(() -> new RuntimeException("Huésped con documento " + documentoId + " no encontrado."));
     }
-
     public Huesped registrar(Huesped huesped) {
         return huespedRepository.save(huesped);
+
     }
 }
