886e146c84437b616ab84e5a4e7984aec0decbda
andre1751 <andrerosales1751@gmail.com>
2026-05-03 21:02:09 -0500

    Update HabitacionService.java

--- src/main/java/com/example/demo/services/HabitacionService.java
+++ src/main/java/com/example/demo/services/HabitacionService.java
@@ -1,28 +1,16 @@
 package com.example.demo.services;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Habitacion;
 import com.example.demo.repository.HabitacionRepo;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class HabitacionService {
-    private static HabitacionService instance;
-
-    @Autowired
-    private HabitacionRepo habitacionRepository;
-
-    private HabitacionService() {
-    }
-    
-    public static HabitacionService getInstance() {
-        if (instance == null) {
-            instance = new HabitacionService();
-        }
-        return instance;
-    }
+    private final HabitacionRepo habitacionRepository;
 
     public List<Habitacion> obtenerTodas() {
         return habitacionRepository.findAll();
